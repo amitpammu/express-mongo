6ed3ad5bce7071e0b98bd50386ddd9dc1a70b9fd
Amit Kumar <amkumar@Avalon>
2018-07-23 14:59:30 +0530

    get todos

--- server/server.js
+++ server/server.js
@@ -22,9 +22,22 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// GET todos
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+
+        res.send({todos});
+
+    }, (e) => {
+        res.status(400).send(e);
+    });
+
+});
+
 //server port
 app.listen('3000', () => {
     console.log("server running..");
 });
 
-module.exports = {app};
+module.exports = { app };
